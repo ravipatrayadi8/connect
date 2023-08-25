32681e0d82d0c699d644d3f9a011503c221f9f8c
ravipatrayadi <ravipatrayadi@gmail.com>
2023-08-25 18:10:13 +0530

    partialcart
    adding and deleting from the cart

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ const path = require("path")
 const hbs = require("hbs")
 const collection = require("./mongodb")
 const app = express()
+const { Collection } = require('collectionsjs');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false})) 
 
-
 app.set("view engine" , "hbs") 
-
 
 app.get("/" , (req,res) =>{
     res.render("login")
@@ -22,6 +21,47 @@ app.get("/login", (req, res) => {
 app.get("/signup", (req, res) => {
     res.render("signup");
 });
+
+function createCounter(items) {
+    const counter = new Map();
+
+    for (const item of items) {
+        const [fruit,price] = item.split('|')
+        if ( price > 0 ){
+            counter.set(item, (counter.get(item) || 0) + 1);
+        }
+        else{
+            const newitem = fruit + '|' + (-price) 
+            if (counter.get(newitem) > 0) {
+                counter.set(newitem, counter.get(newitem) - 1);
+            }
+        }
+    }
+    return counter;
+  }
+
+  function findCost(counterObject) {
+    let cost = 0 ;  
+    for (const key in counterObject) {
+        const [item, price] = key.split('|')         
+        const itemCount = counterObject[key]
+        cost += (price * itemCount)  
+      }
+      return cost ; 
+}
+
+
+const items = [] 
+app.post("/home" , async (req,res) =>{   
+    items.push(req.body.item) 
+    const counter = createCounter(items);
+    const counterObject = Object.fromEntries(counter);
+    const total = findCost(counterObject)
+    console.log(total) 
+    res.render("home") 
+}) 
+
+
 
 
 app.post("/signup" , async (req,res) => {
